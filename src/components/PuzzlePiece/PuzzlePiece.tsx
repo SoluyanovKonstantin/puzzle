8b65bd6725161cc6d7a52a6b338f1e87181fc357
Konstantin Soluianov <dr.chtec@yandex.ru>
2024-01-13 18:12:27 +0400

    add generate puzzles

--- src/components/PuzzlePiece/PuzzlePiece.tsx
+++ src/components/PuzzlePiece/PuzzlePiece.tsx
@@ -1,27 +1,126 @@
-import { FC } from 'react';
+import { FC, useRef, useState } from 'react';
 import style from './PuzzlePiece.module.css';
 
 export type TPuzzlePieceSide = 'in' | 'out' | 'none';
 
 export type TPuzzlePieceProps = {
-  index: number,
+  i: number,
+  j: number,
+  size: number,
   left: TPuzzlePieceSide,
   right: TPuzzlePieceSide,
   top: TPuzzlePieceSide,
   bottom: TPuzzlePieceSide,
+  image?: string
 }
 
-const PuzzlePiece: FC<TPuzzlePieceProps> = ({ index, left, right, top, bottom }) => {
-  let maskImage: string[] = [];
-  let urlToAssets = '/masks/';
+const PuzzlePiece: FC<TPuzzlePieceProps> = ({ i, j, size, left, right, top, bottom, image }) => {
+  const htmlElementWithCirclesRef: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const maskImage: string[] = [];
+  const urlToAssets = '/masks/';
+  const circleRadius = size / 5;
   const sides: Record<string, TPuzzlePieceSide> = { left, right, top, bottom }
   Object.keys(sides).forEach( key => {
-    if (sides[key] !== 'none') {
+    if (sides[key] === 'in') {
       maskImage.push( `url(${urlToAssets}${sides[key]}-${key}.svg)`);
     }
   } );
+
+  const omMouseMove = (event: React.MouseEvent) => { // (1) отследить нажатие
+
+    const ball = event.currentTarget as HTMLElement;
+    ball.ondragstart = () => {
+      return false;
+    };
+    event.preventDefault();
+    // (2) подготовить к перемещению:
+    // разместить поверх остального содержимого и в абсолютных координатах
+    ball.style.position = 'absolute';
+    ball.style.zIndex = '1000';
+    // переместим в body, чтобы мяч был точно не внутри position:relative
+    // и установим абсолютно спозиционированный мяч под курсор
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // передвинуть мяч под координаты курсора
+    // и сдвинуть на половину ширины/высоты для центрирования
+    function moveAt(pageX: number, pageY: number) {
+      const x = pageX - ball.offsetWidth / 2 + 'px';
+      const y = pageY - ball.offsetHeight / 2 + 'px';
+      if (htmlElementWithCirclesRef.current) { htmlElementWithCirclesRef.current.style.top = y };
+      if (htmlElementWithCirclesRef.current) { htmlElementWithCirclesRef.current.style.left = x };
+      ball.style.left = x;
+      ball.style.top = y;
+    }
+  
+    function onMouseMove(event: MouseEvent) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // (3) перемещать по экрану
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // (4) положить мяч, удалить более ненужные обработчики событий
+    ball.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      ball.onmouseup = null;
+    };
+  
+  };
+
   return (
-    <div className={style.block} style={ { maskImage: maskImage.join(', '), WebkitMaskImage: maskImage.join(', ')} } />
+    <>
+      <div onMouseDown={omMouseMove} className={style.block} style={ { 
+        maskImage: maskImage.join(', '), 
+        WebkitMaskImage: maskImage.join(', '), 
+        backgroundImage: `url(${image})`,
+        backgroundPositionX: - size * j, 
+        backgroundPositionY: - size * i,
+        width: size,
+        height: size,
+        position: 'absolute',
+        zIndex: 100000,
+      } } />
+      <div ref={htmlElementWithCirclesRef} style={{ width: size, height: size, position: 'absolute' }}>
+        {left === 'out' && 
+        <div className={[style.block__circle, style.block__circle_left].join(' ')} style={{ 
+          width: size / 2.5, 
+          height: size / 2.5, 
+          backgroundImage: `url(${image})`,
+          backgroundPositionX: -(size * j - circleRadius), 
+          backgroundPositionY: -(size * i + size / 2 - circleRadius),
+          }} />}
+        {right === 'out' &&
+          <div className={[style.block__circle, style.block__circle_right].join(' ')} style={{ 
+          width: size / 2.5, 
+          height: size / 2.5, 
+          backgroundImage: `url(${image})`,
+          backgroundPositionX: -(size * j + size - circleRadius), 
+          backgroundPositionY: -(size * i  + size / 2 - circleRadius),
+          }} />
+          
+        }
+        {top === 'out' &&
+          <div className={[style.block__circle, style.block__circle_top].join(' ')} style={{ 
+          width: size / 2.5, 
+          height: size / 2.5, 
+          backgroundImage: `url(${image})`,
+          backgroundPositionX: -(size * j + size / 2 - circleRadius), 
+          backgroundPositionY: -(size * i - circleRadius),
+          }} />
+        }
+        {bottom === 'out' &&
+          <div className={[style.block__circle, style.block__circle_bottom].join(' ')} style={{ 
+          width: size / 2.5, 
+          height: size / 2.5, 
+          backgroundImage: `url(${image})`,
+          backgroundPositionX: -(size * j + size / 2 - circleRadius), 
+          backgroundPositionY: -(size * i + size - circleRadius),
+          }} />
+        }
+        
+      </div>
+    </>
  );
 }
 
--- src/pages/Editor/Editor.tsx
+++ src/pages/Editor/Editor.tsx
@@ -1,78 +1,119 @@
-import { ReactElement, useState } from 'react';
-import './Editor.css';
-import PuzzlePiece, { TPuzzlePieceProps } from '../../components/PuzzlePiece/PuzzlePiece';
+import { ReactElement, useRef, useState } from "react";
+import "./Editor.css";
+import PuzzlePiece, {
+  TPuzzlePieceProps,
+} from "../../components/PuzzlePiece/PuzzlePiece";
 
-interface Matrix extends HTMLFormControlsCollection   {
-  x: HTMLInputElement;
-  y: HTMLInputElement
+function gcd(a: number, b: number): number {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
 }
- 
-interface MatrixForm extends HTMLFormElement {
-  readonly elements: Matrix;
-}
-
 
 function Editor() {
-  const [url, setUrl] = useState('');
+  const sizeRef = useRef(0);
+  const generateEasyPuzzleRef: React.MutableRefObject<HTMLButtonElement | null> = useRef(null);
+  const generateMiddlePuzzleRef: React.MutableRefObject<HTMLButtonElement | null> = useRef(null);
+  const generateHardPuzzleRef: React.MutableRefObject<HTMLButtonElement | null> = useRef(null);
+  const [url, setUrl] = useState("");
   const [puzzlePieces, setPuzzlePieces] = useState<ReactElement[]>([]);
 
   const onLoadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event);
-    if (event.target.files?.length) {
-      setUrl(URL.createObjectURL(event.target.files[0]));
-    }
-  }
+    const fileReader = new FileReader();
 
-  const generateMatrix = (rows: number, cols: number) => {
+    if (event.target.files?.length) {
+      fileReader.readAsDataURL(event.target.files[0]);
+      fileReader.onloadend = (e) => {
+        const image = new Image();
+        if (e.target) {
+          image.src = fileReader.result as string;
+          setUrl(fileReader.result as string);
+          image.onload = () => {
+            sizeRef.current = gcd(image.width, image.height);
+            if(generateEasyPuzzleRef.current && generateMiddlePuzzleRef.current && generateHardPuzzleRef.current) {
+              generateHardPuzzleRef.current.style.display = generateMiddlePuzzleRef.current.style.display = generateEasyPuzzleRef.current.style.display = 'block';
+              generateEasyPuzzleRef.current.dataset.columns =''+ (image.width / sizeRef.current);
+              generateEasyPuzzleRef.current.dataset.rows =''+ (image.height / sizeRef.current);
+
+              generateMiddlePuzzleRef.current.dataset.columns =''+ (image.width / sizeRef.current / 2);
+              generateMiddlePuzzleRef.current.dataset.rows =''+ (image.height / sizeRef.current / 2);
+
+              generateHardPuzzleRef.current.dataset.columns =''+ (image.width / sizeRef.current / 4);
+              generateHardPuzzleRef.current.dataset.rows =''+ (image.height / sizeRef.current / 4);
+            }
+          };
+        }
+      };
+    }
+  };
+
+  const generateMatrix = (rows: number, cols: number, size: number) => {
     let matrix: TPuzzlePieceProps[][] = [];
-    let index = 0;
-    
+
     for (let i = 0; i < rows; i++) {
       matrix[i] = [];
 
       for (let j = 0; j < cols; j++) {
         const piece: TPuzzlePieceProps = {
-          index,
-          left: j !== 0 ? matrix[i][j - 1].right === 'in' ? 'out' : 'in' : 'none',
-          right: j !== cols - 1 ? Math.random() < 0.5 ? 'in' : 'out' : 'none',
-          bottom: i !== rows - 1 ? Math.random() < 0.5 ? 'in' : 'out' : 'none',
-          top: i !== 0 ? matrix[i - 1][j].bottom === 'in' ? 'out' : 'in': 'none',
-        }
+          i,
+          j,
+          size,
+          left:
+            j !== 0 ? (matrix[i][j - 1].right === "in" ? "out" : "in") : "none",
+          right: j !== cols - 1 ? (Math.random() < 0.5 ? "in" : "out") : "none",
+          bottom:
+            i !== rows - 1 ? (Math.random() < 0.5 ? "in" : "out") : "none",
+          top:
+            i !== 0
+              ? matrix[i - 1][j].bottom === "in"
+                ? "out"
+                : "in"
+              : "none",
+        };
 
         matrix[i][j] = piece;
-        index++;
       }
     }
     return matrix;
-  }
+  };
 
-  const generatePuzzle = (event: React.FormEvent<MatrixForm>) => {
-    event.preventDefault();
-    const target = event.currentTarget.elements;
-
+  const generatePuzzle = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setPuzzlePieces([]);
+    const target = event.currentTarget;
+    console.log(target.dataset.puzzleType);
     const pieces: ReactElement[] = [];
-    const matrix = generateMatrix(+target.x.value, +target.y.value)
-    console.log(matrix);
+    if (target.dataset.rows && target.dataset.columns && target.dataset.puzzleType) {
 
-    matrix.forEach((arr) => arr.forEach((piece) => {
-      pieces.push(<PuzzlePiece {...piece} />)
-    }));
+      const matrix = generateMatrix(+target.dataset.rows, +target.dataset.columns, sizeRef.current / +target.dataset.puzzleType);
 
-    setPuzzlePieces(pieces);
-  }
+      matrix.forEach((arr) =>
+        arr.forEach((piece) => {
+          pieces.push(
+            <PuzzlePiece
+              key={"" + piece.i + "" + piece.j}
+              {...piece}
+              image={url}
+            />
+          );
+        })
+      );
 
-  return <>
-    <input type='file' onChange={onLoadImage} />
-    <img src={ url } alt='puzzle' />
-
-    <form onSubmit={generatePuzzle}>
-      <input name='x' type="number" /> x <input name='y' type="number" />
-      <button type='submit'>submit</button>
-    </form>
-    {
-      puzzlePieces
+      setPuzzlePieces(pieces);
     }
-  </>;
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onLoadImage} />
+      <img src={url} alt="puzzle" />
+      <button ref={generateEasyPuzzleRef} data-puzzle-type='1' onClick={generatePuzzle} style={{display: 'none'}}>generateBigPuzzle</button>
+      <button ref={generateMiddlePuzzleRef} data-puzzle-type='2' onClick={generatePuzzle} style={{display: 'none'}}>generateMiddlePuzzle</button>
+      <button ref={generateHardPuzzleRef} data-puzzle-type='4' onClick={generatePuzzle} style={{display: 'none'}}>generateLittlePuzzle</button>
+      {puzzlePieces}
+    </>
+  );
 }
 
 export default Editor;
